c023b974218c6f8c12e741a8319bfebf5d63a6cb
syedmjavaid <syedmjavaid@gmail.com>
2022-07-04 17:47:39 +0100

    Add like or liked button

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -6,11 +6,26 @@ export default function Artists() {
       <div>Artists</div>
 
       <ul>
-        <li>Drake</li>
-        <li>Bill Withers</li>
-        <li>Pop Smoke</li>
-        <li>Beyonce</li>
-        <li>Rihanna</li>
+        <li>
+          Drake
+          <button>Like</button>
+        </li>
+        <li>
+          Bill Withers
+          <button>Like</button>
+        </li>
+        <li>
+          Pop Smoke
+          <button>Like</button>
+        </li>
+        <li>
+          Beyonce
+          <button>Like</button>
+        </li>
+        <li>
+          Rihanna
+          <button>Like</button>
+        </li>
       </ul>
     </>
   );
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,15 +1,45 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Songs() {
+  const [like, setLike] = useState("Like");
+  const [like2, setLike2] = useState("Like");
+  const [like3, setLike3] = useState("Like");
+  const [like4, setLike4] = useState("Like");
+
+  function handleLike(e) {
+    // setLike("Liked");
+    if (e.target.innerText === "Liked") {
+      e.target.innerText = "Like";
+    } else {
+      e.target.innerText = "Liked";
+    }
+  }
+
   return (
     <>
       <div>Songs</div>
       <ul>
-        <li>In my feelings</li>
-        <li>Ain't no sunshine when shes gone</li>
-        <li>Dior</li>
-        <li>Crazy in love</li>
-        <li>Umbrella</li>
+        <li>
+          In my feelings
+          <button onClick={handleLike}>{like}</button>
+        </li>
+        <li>
+          Ain't no sunshine when shes gone
+          <button onClick={handleLike}>Like</button>
+        </li>
+        <li>
+          Dior
+          <button onClick={handleLike}>Like</button>
+        </li>
+        <li>
+          Crazy in love
+          <button onClick={handleLike}>Like</button>
+        </li>
+        <li>
+          Umbrella
+          <button onClick={handleLike}>Like</button>
+        </li>
       </ul>
     </>
   );
