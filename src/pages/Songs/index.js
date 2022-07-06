882348a35ad767d22889fd585996a8392a26c53f
syedmjavaid <syedmjavaid@gmail.com>
2022-07-06 16:04:41 +0100

    Complete useContext refactoring

--- src/pages/Songs/index.js
+++ src/pages/Songs/index.js
@@ -1,51 +1,19 @@
-import React from 'react';
-import {SongList, Lyric} from '../../components'
-import { useState } from 'react';
+import React, { useContext } from "react";
+import { SongList, Lyric } from "../../components";
+import { useState } from "react";
+import { SongContext } from "../../contexts/SongContext";
 
 const Songs = () => {
+  const [songs, setSong] = useContext(SongContext);
 
-    const [songs, setSong] = useState([
-        {
-          name: "In my feelings",
-          artist: "Drake",
-          like: false,
-          id: Math.random() * 1000,
-        },
-        {
-          name: "Ain't No Sunshine",
-          artist: "Bill Withers",
-          like: false,
-          id: Math.random() * 1000,
-        },
-        {
-          name: "Dior",
-          artist: "Pop Smoke",
-          like: false,
-          id: Math.random() * 1000,
-        },
-        {
-          name: "Crazy in love",
-          artist: "Beyonce",
-          like: false,
-          id: Math.random() * 1000,
-        },
-        {
-          name: "Umbrella",
-          artist: "Rihanna",
-          like: false,
-          id: Math.random() * 1000,
-        },
-      ]);
-      
-    const [songLyric, setSongLyric] = useState([]);
-    return (
-
-        <>
-        <h1>Songs page</h1>
-        <SongList  songs={songs} setSong={setSong} setSongLyric={setSongLyric} />
-        <Lyric songLyric={songLyric} />
-        </>
-    )
-}
+  const [songLyric, setSongLyric] = useState([]);
+  return (
+    <>
+      <h1>Songs page</h1>
+      <SongList songs={songs} setSong={setSong} setSongLyric={setSongLyric} />
+      <Lyric songLyric={songLyric} />
+    </>
+  );
+};
 
 export default Songs;
